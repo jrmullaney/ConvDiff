0dfd37230e36a96e7f32481850b922eeb5cce7c8
James Mullaney <j.mullaney@sheffield.ac.uk>
2020-06-01 16:54:57 +0100

    Varying intensity

--- code/makeDataset.py
+++ code/makeDataset.py
@@ -18,6 +18,7 @@ class SampleDataset(Dataset):
         for i in range(images.shape[0]):
             
             pos = np.random.uniform(low=0, high=image_size-1, size=[6,2])
+            intensity = np.random.uniform(low=5, high=15, size=6)
             trans = np.random.normal(0,3,2) if translation else np.zeros(2)
             
             fwhm = np.array([3,3])#)np.random.normal(3,0.75,2)
@@ -25,20 +26,20 @@ class SampleDataset(Dataset):
 
             for j in range(pos.shape[0]-1):
                 images[i,0,...] += Gaussian2D(
-                    10, 
+                    intensity[j], 
                     pos[j,0], pos[j,1], 
                     fwhm[0], fwhm[0], 
                     theta=0.5
                     )(x, y)
                 images[i,1,...] += Gaussian2D(
-                    10, 
+                    intensity[j], 
                     pos[j,0] + trans[0], pos[j,1] + trans[1],
                     fwhm[1], fwhm[1], 
                     theta=0.5
                     )(x, y)
         
             extra = Gaussian2D(
-                10, 
+                intensity[5], 
                 pos[5,0] + trans[0], pos[5,1] + trans[1],
                 fwhm[1], fwhm[1],
                 theta=0.5
