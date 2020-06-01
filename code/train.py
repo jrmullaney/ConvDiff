e61d88cf99d5cb769cc2c018a471c51d90bb169f
James Mullaney <j.mullaney@sheffield.ac.uk>
2020-06-01 12:42:31 +0100

    Edited train.py to account for changes in makeDataset.py

--- code/train.py
+++ code/train.py
@@ -15,8 +15,8 @@ criterion = nn.L1Loss()
 optimizer = optim.Adam(net.parameters())
 
 # Generate and load the dataset
-dataset = SampleDataset(imsize=128)
-loader = DataLoader(dataset,batch_size=2, shuffle=True, num_workers=2)
+dataset = SampleDataset(n_images=200, image_size=128)
+loader = DataLoader(dataset, batch_size=2, shuffle=True, num_workers=2)
 
 for epoch in range(2):
 
