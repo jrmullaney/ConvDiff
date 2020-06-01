9cc89f5dd5c6b8fac78ddcf20e165e1ce116bb69
James Mullaney <j.mullaney@sheffield.ac.uk>
2020-06-01 16:48:49 +0100

    Using constant PSF

--- code/makeDataset.py
+++ code/makeDataset.py
@@ -20,25 +20,25 @@ class SampleDataset(Dataset):
             pos = np.random.uniform(low=0, high=image_size-1, size=[6,2])
             trans = np.random.normal(0,3,2) if translation else np.zeros(2)
             
-            fwhm = np.random.normal(3,0.75,2)
+            fwhm = np.array([3,3])#)np.random.normal(3,0.75,2)
             fwhm[1] = fwhm[1] if vary_psf else fwhm[0]
 
             for j in range(pos.shape[0]-1):
                 images[i,0,...] += Gaussian2D(
-                    5, 
+                    10, 
                     pos[j,0], pos[j,1], 
                     fwhm[0], fwhm[0], 
                     theta=0.5
                     )(x, y)
                 images[i,1,...] += Gaussian2D(
-                    5, 
+                    10, 
                     pos[j,0] + trans[0], pos[j,1] + trans[1],
                     fwhm[1], fwhm[1], 
                     theta=0.5
                     )(x, y)
         
             extra = Gaussian2D(
-                5, 
+                10, 
                 pos[5,0] + trans[0], pos[5,1] + trans[1],
                 fwhm[1], fwhm[1],
                 theta=0.5
