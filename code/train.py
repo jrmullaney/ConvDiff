752e7b091e5ec7cd67d8d4d688979655fc88350e
jrmullaney <j.mullaney@sheffield.ac.uk>
2020-06-19 11:42:45 +0100

    Reduced transientboost=4

--- code/train.py
+++ code/train.py
@@ -29,7 +29,7 @@ for epoch in range(2):
         src = focus > 0
         transient = focus == 2
         src_boost = 5.
-        transient_boost = 10. # Total boost = src_boost * transient_boost
+        transient_boost = 4. # Total boost = src_boost * transient_boost
 
         truth[src] = src_boost * truth[src]
         truth[transient] = transient_boost * truth[transient] 
